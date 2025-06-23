f0b2d0029e35095a7db4f8a8e5c64a45fd2c0e83
Ninja0_0 <heshamgamal2582@gmail.com>
2025-06-23 09:05:28 +0300

    Updated NormalUserView.tsx

--- src/components/NormalUserView.tsx
+++ src/components/NormalUserView.tsx
@@ -1168,7 +1168,7 @@ const NormalUserView: React.FC<NormalUserViewProps> = ({ section }) => {
                   }}
                   className="px-6 py-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 transition-colors"
                 >
-                  Cancel
+                  إلغاء
                 </button>
               </div>
             </div>
