5f90f0e123bab8582016b0e274c89e3c439c2385
Ninja0_0 <heshamgamal2582@gmail.com>
2025-06-23 09:09:26 +0300

    Updated NormalUserView.tsx

--- src/components/NormalUserView.tsx
+++ src/components/NormalUserView.tsx
@@ -1251,7 +1251,7 @@ const NormalUserView: React.FC<NormalUserViewProps> = ({ section }) => {
                   />
                   {activeShift && (
                     <p className="text-xs text-gray-500 mt-1">
-                      المتوفرة: {activeShift.totalAmount} EGP
+                      النقدية المتوفرة: {activeShift.totalAmount} EGP
                     </p>
                   )}
                 </div>
