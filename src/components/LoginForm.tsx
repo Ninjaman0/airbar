50e1295b051064fa539b103fc079f7c95a891695
Ninja0_0 <heshamgamal2582@gmail.com>
2025-06-23 07:50:41 +0300

    Updated LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -86,7 +86,7 @@ const LoginForm: React.FC = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full pl-10 pr-12 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
-                placeholder="Enter your password"
+                placeholder="أدخل كلمة المرور"
                 disabled={isLoading}
                 autoComplete="current-password"
                 required
