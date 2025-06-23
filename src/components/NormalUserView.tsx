a66adf5a48f2d9a8760569ce8610ef92b553009a
Ninja0_0 <heshamgamal2582@gmail.com>
2025-06-23 08:19:18 +0300

    Updated NormalUserView.tsx

--- src/components/NormalUserView.tsx
+++ src/components/NormalUserView.tsx
@@ -614,7 +614,7 @@ const NormalUserView: React.FC<NormalUserViewProps> = ({ section }) => {
                 <thead className="bg-gray-50">
                   <tr>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">الصنف</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">الكميه</th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total Price</th>
                   </tr>
                 </thead>
