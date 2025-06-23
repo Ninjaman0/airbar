e630808b9ee7b4d416860c0c78bad7f96d912838
Ninja0_0 <heshamgamal2582@gmail.com>
2025-06-23 09:22:37 +0300

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,7 @@ const Layout: React.FC<LayoutProps> = ({ children, currentSection, onSectionChan
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-4">
               <div className="bg-blue-100 p-2 rounded-lg">
-                <Store className="h-6 w-6 text-blue-600" />
+                <Dumbbell className="h-6 w-6 text-blue-600" />
               </div>
               <h1 className="text-xl font-bold text-gray-900">AIR BAR</h1>
             </div>
