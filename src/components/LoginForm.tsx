ed7a212190ada4838f6d30cea8feaaa9693ea092
Ninja0_0 <heshamgamal2582@gmail.com>
2025-06-23 07:52:29 +0300

    Updated LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -126,7 +126,7 @@ const LoginForm: React.FC = () => {
 
         <div className="mt-6 text-center">
           <p className="text-xs text-gray-500">
-            Air Gym bar system if you found it by mistake you you can fuck off :)
+            Air Gym bar system if you found it by mistake you can fuck off :)
           </p>
         </div>
       </div>
